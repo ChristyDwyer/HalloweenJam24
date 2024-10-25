6da4833104866d0095b20e486dcb262f80b616a5
Tom Roberts <Tom_A_Roberts@outlook.com>
2024-10-25 14:51:52 +0100

    Global Volume Settings
    Added the background functions for master volume control pending UI design choices.

---
+++ HalloweenJam24/Assets/Scripts/UIScripts/MenuScripts/Settings.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+
+    [SerializeField] private float volume;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (PlayerPrefs.GetInt("SettingsEnabled"))
+        {
+            if (PlayerPrefs.HasKey("GlobalVolume")) { volume = PlayerPrefs.GetFloat("GlobalVolume"); }
+            Debug.Log("Hi I'm starting and the volume is "+ volume);
+            PlayerPrefs.SetInt("SettingsEnabled", 1);
+            PlayerPrefs.SetFloat("GlobalVolume", volume);
+            AudioListener.volume = (PlayerPrefs.GetFloat("GlobalVolume"));
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (AudioListener.volume != volume)
+        {
+            PlayerPrefs.SetFloat("GlobalVolume", volume);
+            Debug.Log("Volume changed to "+ AudioListener.volume);
+            AudioListener.volume = (PlayerPrefs.GetFloat("GlobalVolume"));
+        }
+    }
+}
