8a458bfe1b8bb050bd86a65dd57e6716d9a61505
Tom Roberts <Tom_A_Roberts@outlook.com>
2024-10-25 21:45:08 +0100

    Bug Fix Settings Manager
    Set default values for volume removed extra script causing errors.

--- HalloweenJam24/Assets/Scripts/UIScripts/MenuScripts/Settings.cs
+++ HalloweenJam24/Assets/Scripts/UIScripts/MenuScripts/Settings.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class Settings : MonoBehaviour
 {
 
-    [SerializeField] private float volume;
+    [SerializeField] private float volume = 0.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //if (PlayerPrefs.GetInt("SettingsEnabled"))
         {
-            if (PlayerPrefs.HasKey("GlobalVolume")) { volume = PlayerPrefs.GetFloat("GlobalVolume"); }
+            if (PlayerPrefs.HasKey("GlobalVolume"))
+            {
+                volume = PlayerPrefs.GetFloat("GlobalVolume");
+            }
+            else volume = 0.01f;
             Debug.Log("Hi I'm starting and the volume is "+ volume);
             PlayerPrefs.SetInt("SettingsEnabled", 1);
-            PlayerPrefs.SetFloat("GlobalVolume", volume);
             AudioListener.volume = (PlayerPrefs.GetFloat("GlobalVolume"));
+            PlayerPrefs.Save();
         }
 
     }
@@ -26,9 +29,13 @@ public class Settings : MonoBehaviour
     {
         if (AudioListener.volume != volume)
         {
+           
             PlayerPrefs.SetFloat("GlobalVolume", volume);
-            Debug.Log("Volume changed to "+ AudioListener.volume);
-            AudioListener.volume = (PlayerPrefs.GetFloat("GlobalVolume"));
+            float temp = PlayerPrefs.GetFloat("GlobalVolume");
+            AudioListener.volume = temp;
+            Debug.Log(temp);
+
+            PlayerPrefs.Save();
         }
     }
 }
