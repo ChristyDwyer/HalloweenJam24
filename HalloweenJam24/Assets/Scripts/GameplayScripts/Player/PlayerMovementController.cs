e791f1ac1767fa24716ceff784ad529d63d732bc
Marcio Ferreira <13ferreirama@gmail.com>
2024-10-29 17:39:39 +0000

    Action Maps and Broken Controls
    Tried to implement player controller using new input system but had some unforseen complications

---
+++ HalloweenJam24/Assets/Scripts/GameplayScripts/Player/PlayerMovementController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public HalloweenJam24 controls;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
